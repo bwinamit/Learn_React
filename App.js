029dd4086ffd6e6ce3205a7886502d9195ff0e0d
bwinamit <bwinamit19@gmail.com>
2025-03-26 20:58:42 +0530

    InDepth learning about parcel,react and reactDOm

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client"; 
+
 const parent=
     React.createElement("div", {id:"parent"},
     React.createElement("div", {id:"child"},[
-    React.createElement("h1", {}, "Hello World from React H1 Tag!"),
-    React.createElement("h2", {}, "Hello World from React H2 Tag!")
+    React.createElement("h1", {key: "h1"}, "Hello World from React Amit Tag!"),
+    React.createElement("h2", {key: "h2"}, "Hello World from React H2 Tag!")
 ]),
 React.createElement("div", {id:"child2"},[
-    React.createElement("h1", {}, "Hello World from React H1 Tag!"),
-    React.createElement("h2", {}, "Hello World from React H2 Tag!")
+    React.createElement("h1", {key: "h3"}, "Hello World from React H1 Tag!"),
+    React.createElement("h2", {key: "h4"}, "Hello World from React H2 Tag!")
 ]),
 );
 
