3d5da2e84fc9e2ba90e2a932fa878cf0b0a8236a
bwinamit <bwinamit19@gmail.com>
2025-04-30 19:22:13 +0530

    Unit Testing started

--- SRC/components/__tests__/Contact.test.js
+++ SRC/components/__tests__/Contact.test.js
@@ -2,9 +2,23 @@ import { render, screen } from "@testing-library/react";
 import Contact from "../Contact";
 import '@testing-library/jest-dom';
 
+describe("Contact Us Component", () => {
+  test("should load contact us component", () => {
+    //Render
+    render(<Contact />);
+    //Querying
+    const heading = screen.getByRole("heading", { name: /contact us/i });
+    //Assertion
+    expect(heading).toBeInTheDocument();
+  });
+  
+  it("should load contact us component with correct text", () => {
+    //Render
+    render(<Contact />);
+    //Querying
+    const text = screen.getByText(/we would love to hear from you/i);
+    //Assertion
+    expect(text).toBeInTheDocument();
+  })
+})
 
-test("should load contact us component", () => {
-  render(<Contact />);
-  const heading = screen.getByRole("heading", { name: /contact us/i });
-  expect(heading).toBeInTheDocument();
-});
